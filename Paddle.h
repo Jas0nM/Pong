1b3b90c627d2de930e283612f8af35558967f1c5
Jas0nM <112782374+Jas0nM@users.noreply.github.com>
2022-09-22 10:16:15 -0500

    Create Paddle.h

---
+++ Paddle.h
@@ -0,0 +1,14 @@
+#pragma once
+class Paddle
+{
+	private:
+		static const float moveSpeed;
+		unsigned int sprite;
+		float x, y;
+		void move(float moveDirection);
+	public:
+		Paddle(unsigned int image, float startX);
+		void moveUP();
+		void moveDown();
+		void reset();
+};
