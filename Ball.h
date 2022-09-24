b2ad29d4f63311b0a070cf70e074810f07612b7e
Jas0nM <112782374+Jas0nM@users.noreply.github.com>
2022-09-24 13:39:19 -0500

    Create Ball.h

---
+++ Ball.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Paddle;
+
+class Ball
+{
+	private:
+		static const float moveSpeed;
+		unsigned int sprite, bounceSound;
+		float x, y, velX, velY;
+		Paddle* paddles[2];
+		bool collidingWithPaddle;
+
+	public:
+		Ball(Paddle* leftPaddle, Paddle* rightPaddle);
+		void update();
+};
