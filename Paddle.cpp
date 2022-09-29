07a0fd95477f5e4e2b702251eec2d5b7844763d9
Jas0nM <112782374+Jas0nM@users.noreply.github.com>
2022-09-29 11:09:15 -0500

    Update Paddle.cpp

--- Paddle.cpp
+++ Paddle.cpp
@@ -1,33 +1,49 @@
 #include "Paddle.h"
 #include "agk.h"
 
-const float Paddle::moveSpeed = 350.0f;
+//Variable con la que se indica la velocidad que tendran los objetos de esta clase
+const float Paddle::moveSpeed = 360.0f;
 
+//Constructor
 Paddle::Paddle(unsigned int image, float startX)
 {
+	//Variable que guardar la imagen que tendra el sprite
 	sprite = agk::CreateSprite(image);
+
+	//Variables para posicionar al sprite
 	x = startX;
-	y = agk::GetVirtualHeight() / 2.0f;
+	y = agk::GetVirtualHeight()/2.0f;
+
+	//Función para actualizar la posición del sprite
 	agk::SetSpritePositionByOffset(sprite, x, y);
 }
 
-void Paddle::moveUP() 
+//Función para indicar un movimiento hacia arriba
+void Paddle::moveUp() 
 {
 	move(-1.0f);
 }
 
+//Función para indicar un movimiento hacia abajo
 void Paddle::moveDown() 
 {
 	move(1.0f);
 }
 
+//Función con la que se calcula el movimiento que tendran los objetos de la clase
 void Paddle::move(float moveDirection)
 {
+	//Variable regulada que indica el movimiento que tendran los objetos
 	const float moveY = (moveSpeed * moveDirection) * agk::GetFrameTime();
+	
+	//Variable que contiene el valor actualizado que se le dara a la posición en Y
 	const float newY = y + moveY;
+
+	//Variables de posición minima y maxima en el eje Y para los objetos de la clase
 	const float minY = agk::GetSpriteHeight(sprite) / 2.0f;
 	const float maxY = agk::GetVirtualHeight() - minY;
 
+	//Condición con la que se evita que los objetos salgan de la pantalla de ejecución
 	if (newY < minY)
 	{
 		y = minY;
@@ -41,15 +57,24 @@ void Paddle::move(float moveDirection)
 		y = newY;
 	}
 
+	//Función para actualizar la posición del sprite
 	agk::SetSpritePositionByOffset(sprite, x, y);
 }
 
+//Función para posicionar a los objetos como al inicio de la ejecución
 void Paddle::reset()
 {
 	agk::SetSpritePositionByOffset(sprite, x, agk::GetVirtualHeight()/2.0f);
 }
 
+//Función que verifica si el objeto esta colisionando con el sprite indicado como parametro
 bool Paddle::isCollidingWhithSprite(unsigned int otherSprite) const
 {
 	return 1 == agk::GetSpriteCollision(sprite, otherSprite);
+}
+
+//Función que devuelve la posición en Y del sprite
+float Paddle::getY() const
+{
+	return agk::GetSpriteYByOffset(sprite);
 }
